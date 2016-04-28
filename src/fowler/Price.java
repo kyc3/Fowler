121b7f8fb258ca8e3da284a8c1e4d4faf76604ea
Yannick Winter <yannick.winter@sap.com>
2016-04-28 12:11:18 +0200

    Move Method getFrequentRenterPoints to price

--- src/fowler/Price.java
+++ src/fowler/Price.java
@@ -3,4 +3,9 @@ package fowler;
 public abstract class Price {
 	abstract int getPriceCode();
 	abstract double getCharge(int daysRented);
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		 // add bonus for a two day new release rental
+		return 1;
+	}
 }
